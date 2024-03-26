772409508ed0fbd83a7ced3e2c2b6e1d88d40e2f
Abdullah Zafar <abdzafar336@gmail.com>
2024-03-26 14:37:02 +0500

    first commit

---
+++ pages/accommodations/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Accommodations</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="js">
+import { ref } from "vue";
+// import { Calendar } from "vue-calendar-3";
+// import "vue-calendar-3/dist/index.css";
+
+const checkIn = ref(null);
+const checkOut = ref(null);
+</script>
+
+<template>
+  <!-- <Calendar v-model:checkIn="checkIn" v-model:checkOut="checkOut" /> -->
+  <h1>helo</h1>
+</template>
